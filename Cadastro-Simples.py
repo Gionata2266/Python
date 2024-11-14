d1918d07e5c945d40aae2cb47f912bab15b003b2
Gionata2266 <gionata2266@hotmail.com>
2024-11-14 14:47:06 -0300

    começo do fim

--- Cadastro-Simples.py
+++ Cadastro-Simples.py
@@ -3,34 +3,33 @@ while(True):
   if(len(nome )>3):
     break
   else:
-    print("Nome inválido. Digite novamente.")
+    print("Nome invÃ¡lido. Digite novamente.")
 
 while (True):
     idade = int(input("Digite a idade: "))
     if (0 <= idade <= 150):
       break
     else:
-      print("Idade inválida. Digite novamente.") 
+      print("Idade invÃ¡lida. Digite novamente.") 
       
 while(True):
   salario = float(input("Digite o Salario"))
   if(salario > 0):
     break
   else:
-    print("salario inválido. Digite novamente.") 
+    print("salario invÃ¡lido. Digite novamente.") 
     
 while(True):
   sexo = str(input("Digite Sexo. (M) ou (F)").lower())
   if(sexo in ['f','m']):    
     break
   else:
-    print("sexo inválido. Digite novamente.") 
-    while(True):
+    print("sexo invÃ¡lido. Digite novamente.") 
+while(True):
   civil = str(input("Digite estado civil. (S) ou (D) ou (C) ou (V)").lower())
   if(civil in ['s','d','c','v']):    
     break
   else:
-    print("sexo inválido. Digite novamente.") 
+    print("sexo invÃ¡lido. Digite novamente.") 
 
 print(f"NOME:{nome} IDADE:{idade} SALARIO:{salario} SEXO:{sexo} ESTADO-CIVIL:{civil}")
-
